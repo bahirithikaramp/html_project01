93e3750de93aa4907a1f0028b78923b4e25a6e14
bahirithikaramp <bahirithi.karampudi@gmail.com>
2020-08-20 23:53:34 +0530

    Program to find if a SLL contains a loop and return the starting point of the loop

---
+++ list_cycle.cpp
@@ -0,0 +1,51 @@
+/*Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+Try solving it using constant additional space.*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ *
+ * typedef struct ListNode listnode;
+ *
+ * listnode* listnode_new(int val) {
+ *     listnode* node = (listnode *) malloc(sizeof(listnode));
+ *     node->val = val;
+ *     node->next = NULL;
+ *     return node;
+ * }
+ */
+/**
+ * @input A : Head pointer of linked list
+ *
+ * @Output head pointer of list.
+ */
+listnode* detectCycle(listnode* A) {
+    int loop = 0;
+    listnode *slow,*fast;
+    slow = fast = A;
+    while(fast&&fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast)
+        {
+            loop+=1;
+            break;
+        }
+    }
+    if(loop)
+    {
+        slow = A;
+        while(slow!=fast)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+    return NULL;
+}
